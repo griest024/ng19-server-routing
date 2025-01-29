1a47863e55bb43ab3888ef2fbef2b049925e060f
Peter Lauck <griest024@gmail.com>
2025-01-29 23:10:44 +0000

    add routes

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -1,8 +1,6 @@
 import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
 import { provideServerRendering } from '@angular/platform-server';
-import { provideServerRoutesConfig } from '@angular/ssr';
 import { appConfig } from './app.config';
-import { serverRoutes } from './app.routes.server';
 
 const serverConfig: ApplicationConfig = {
   providers: [
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,27 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+@Component({selector: 'test', template: 'test'})
+class TestComponent {}
+@Component({selector: 'not-found', template: '404 not found'})
+class NotFoundComponent {}
+
+export const routes: Routes = [
+  {
+    path: '',
+    children: [
+      {
+        path: 'not-found',
+        component: NotFoundComponent
+      },
+      {
+        path: '**',
+        component: TestComponent
+      },
+    ]
+  },
+  {
+    path: '**',
+    redirectTo: 'not-found'
+  },
+];
